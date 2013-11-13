43d096442305746bdaadd6e83e7b6d1209789e43
Raymon White <raymonwhite42@gmail.com>
2013-11-13 08:50:21 +0000

    New run.sh format

--- lagrange_bifurcation/run.sh
+++ lagrange_bifurcation/run.sh
@@ -1,20 +1,56 @@
 #!/bin/bash
 
-# Use
-# tetgen -a0.2 fsi_bifurcation_fluid.poly
-# to generate the mesh.
-# 0.2 is the maximum area/volumne of the elements.
-# decrease it to get a finer mesh.
-
 ## PROGRAM NAME
 PROGRAM="unstructured_three_d_fluid"
-RESLTDIR="RESLT_TH"
+AUTOGENSCRIPT="autogen_wipebuild_noselftest.sh"
 
-touch ./$RESLTDIR && rm -rf ./$RESLTDIR && mkdir $RESLTDIR && \
-make $PROGRAM && \
-./$PROGRAM --w_solver 0 --ns_solver 0 --rey 200 --doc_soln $RESLTDIR && \
-cd $RESLTDIR && \
-../plot_it.bash && \
-paraview soln.pvd
+## Directories
+RESLTDIR="RESLT"
+DOCPRECDIR="prec_data"
+CURRENTDIR=`pwd`
+OOMPH_ROOT_DIR=$(make -s --no-print-directory print-top_builddir)
+
+
+CMD_INPUT="--w_solver 0 --ns_solver 1 --visc 0 --rey 0 --mesh_type 0 --mesh_area 0.0125 --ntsteps 4 --doc_prec $DOCPRECDIR"
+
+# Setup directories
+function setup_dir
+{
+  touch ./$RESLTDIR && rm -rf ./$RESLTDIR && mkdir $RESLTDIR && \
+  touch ./$DOCPRECDIR && rm -rf ./$DOCPRECDIR && mkdir $DOCPRECDIR
+}
+
+function make_src
+{
+  cd $OOMPH_ROOT_DIR/src && \
+  make && make install && \
+  cd $CURRENTDIR
+
+#  cd $OOMPH_ROOT_DIR && \
+#  ./$AUTOGENSCRIPT --jobs=4 --rebuild && \
+#  cd $CURRENTDIR
+}
+
+function make_run_program
+{
+  make $PROGRAM && \
+  mpirun ./$PROGRAM $CMD_INPUT
+}
+
+###############################################################################
+###############################################################################
+#setup_dir && make_src && make_run_program
+make_run_program
+
+
+
+###############################################################################
+###############################################################################
+
+#make $PROGRAM && \
+#./$PROGRAM --w_solver 0 --ns_solver 0 --rey 100 --doc_soln $RESLTDIR && \
+#cd $RESLTDIR && \
+#../plot_it.bash && \
+#paraview soln.pvd
 
 
